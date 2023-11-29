a2d7c7c4db75e99c60d3d85fe8304e7ae15a4c81
murodrap <tstrisovska@gmail.com>
2023-11-29 19:05:52 +0100

    priprava evaluate pre indexation

--- Assets/Scripts/AnimationControl/EXEASTNodeIndexation.cs
+++ Assets/Scripts/AnimationControl/EXEASTNodeIndexation.cs
@@ -24,88 +24,46 @@ namespace OALProgramControl
 
             this.EvaluationState = EEvaluationState.IsBeingEvaluated;
 
-            if (valueContext == null || valueContext.CurrentValue == null)
+            EXEExecutionResult operandExecutionResult;
+            EXEExecutionResult operatorExecutionResult;
+
+            operandExecutionResult = List.Evaluate(currentScope, currentProgramInstance);
+
+            if (!operandExecutionResult.IsSuccess)
             {
-                // We want to access a method of the current method owning object
-                EXEVariable selfVariable = currentScope.FindVariable(EXETypes.SelfReferenceName);
-                OwningObject = selfVariable.Value;
-                valueContext = new EXEASTNodeAccessChainContext() { CurrentAccessChain = string.Empty };
-            }
-            else
-            {
-                // We want to access a method of an object given by the context
-                OwningObject = valueContext.CurrentValue;
+                this.EvaluationState = EEvaluationState.HasBeenEvaluated;
+                this.EvaluationResult = operandExecutionResult;
             }
 
-            if (OwningObject.MethodExists(this.MethodName))
-            {
-                // Method exists
-                Method = OwningObject.FindMethod(this.MethodName);
-            }
-            else
-            {
-                // Method does not exist, time to raise an error
-                return EXEExecutionResult.Error(ErrorMessage.MethodNotFoundOnClass(this.MethodName, this.OwningObject.TypeName), "XEC2015");
-            }
 
-            if (this.Arguments.Count != this.Method.Parameters.Count)
-            {
-                return EXEExecutionResult.Error
-                    (
-                        ErrorMessage.InvalidParameterCount
-                        (
-                            this.Method.OwningClass.Name,
-                            this.MethodName,
-                            this.Method.Parameters.Count,
-                            this.Arguments.Count
-                        ),
-                        "XEC2016"
-                    );
-            }
-
-            // Now, let us evaluate the args and invoke the method
-            EXEExecutionResult argumentExecutionResult;
-            for (int i = 0; i < this.Arguments.Count; i++)
-            {
-                argumentExecutionResult = this.Arguments[i].Evaluate(currentScope, currentProgramInstance);
-
-                if (!argumentExecutionResult.IsSuccess)
+                // Current operand evaluation finished and did not produce an error
+                if (this.EvaluationResult == null)
                 {
+                    this.EvaluationResult = EXEExecutionResult.Success();
+                    this.EvaluationResult.ReturnedOutput = operandExecutionResult.ReturnedOutput;
+                    continue;
+                }
+
+                operatorExecutionResult = this.EvaluationResult.ReturnedOutput.ApplyOperator(this.Operation, operandExecutionResult.ReturnedOutput);
+
+                if (!operatorExecutionResult.IsSuccess)
+                {
+                    this.EvaluationResult = operatorExecutionResult;
                     this.EvaluationState = EEvaluationState.HasBeenEvaluated;
-                    this.EvaluationResult = argumentExecutionResult;
                 }
 
-                if (!argumentExecutionResult.IsDone || !argumentExecutionResult.IsSuccess)
+                if (!operatorExecutionResult.IsDone || !operatorExecutionResult.IsSuccess)
                 {
-                    // Either current argument evaluation did not finish or produced an error
-                    return argumentExecutionResult;
+                    // Either current operand evaluation did not finish or produced an error
+                    return operatorExecutionResult;
                 }
 
-                // Current argument evaluation finished and did not produce an error
+                this.EvaluationResult = EXEExecutionResult.Success();
+                this.EvaluationResult.ReturnedOutput = operatorExecutionResult.ReturnedOutput;
+            
 
-                // Check if the returned value matches the expected parameter type
-                if (!EXETypes.CanBeAssignedTo(argumentExecutionResult.ReturnedOutput, this.Method.Parameters[i].Type, currentProgramInstance.ExecutionSpace))
-                {
-                    VisitorCommandToString visitor = VisitorCommandToString.BorrowAVisitor();
-                    argumentExecutionResult.ReturnedOutput.Accept(visitor);
-                    return EXEExecutionResult.Error
-                    (
-                        ErrorMessage.InvalidParameterValue
-                        (
-                            this.Method.OwningClass.Name,
-                            this.MethodName,
-                            this.Method.Parameters[i].Name,
-                            this.Method.Parameters[i].Type, visitor.GetCommandStringAndResetStateNow()
-                        ),
-                        "XEC2017"
-                    );
-                }
-            }
+            this.EvaluationState = EEvaluationState.HasBeenEvaluated;
 
-            // All arguments have been evaluated and without an error
-            this.ExpectingReturn = true;
-            this.EvaluationResult = EXEExecutionResult.Success();
-            this.EvaluationResult.PendingCommand = new EXECommandCall(OwningObject, valueContext.CurrentAccessChain, this);
             return this.EvaluationResult;
         }
 
